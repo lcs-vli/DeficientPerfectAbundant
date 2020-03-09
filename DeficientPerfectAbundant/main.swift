97fedbbeec3e045f6e36f35c8fddd02cd2efca4c
Victoria Li <VLi@lcs.on.ca>
2020-03-09 11:11:13 -0400

    Initial Commit

---
+++ DeficientPerfectAbundant/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DeficientPerfectAbundant
+//
+//  Created by Li, Muchen on 2020-03-09.
+//  Copyright © 2020 Li, Muchen. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
