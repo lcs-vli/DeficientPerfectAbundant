a28cb6137662a2c1e0ae9a407a0b291d2a638e86
Victoria Li <VLi@lcs.on.ca>
2020-03-11 10:49:48 -0400

    fixed the problem (sometimes correct and sometimes not) by moving the two variables inside the while true

--- DeficientPerfectAbundant/main.swift
+++ DeficientPerfectAbundant/main.swift
@@ -8,15 +8,15 @@
 
 import Foundation
 
-
-//
-// INPUT SECTION OF PROGRAM
-//
-var total = 0
-var integer = -1
-
 // Loop until valid input provided by user
 while true {
+    
+    //
+    // INPUT SECTION OF PROGRAM
+    //
+    var total = 0
+    var integer = -1
+    
     
     // Ask for input
     print("What is the number?")
@@ -46,19 +46,22 @@ while true {
     // If we've made it here, the input is valid
     integer = integerGiven
     
+    //loop over all the possible values
     for x in 1...32500 {
         
+        //if the number is a possible diviser, add the number to total
         if integer % x == 0{
             total += x
         }
         
     }
 
+    //compare the total vlue to the integer
     if total-integer < integer {
         print("\(integer) is a deficeint number")
     } else if total-integer == integer{
         print("\(integer) is a perfect number")
-    } else {
+    } else if total-integer > integer{
        print("\(integer) is a abundant number")
     }
     
